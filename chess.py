366e16a488a55aae1c4a2f4f3cd09518c64eefdd
Olive Daly <olive-branch@posteo.me>
2021-05-21 12:00:37 -0400

    Begin refactoring code to use Position class which combines numpy.array/vector coordinates and chess notation into a single object

--- chess.py
+++ chess.py
@@ -1,3 +1,34 @@
+import numpy
+import piece_moves
+
+validate_coordinates = piece_moves.validate_coordinates
+
+class Position:
+    def __init__(self, position):
+        XVALUES = list("ABCDEFGH")
+        YVALUES = list("87654321")
+        if isinstance(position, str):
+            #POSITION CAN BE PROVIDED AS A STRING LIKE "A2"
+            position = position.upper()
+            self.chess_notation = position
+            x = XVALUES.index(position[0])
+            y = YVALUES.index(position[1])
+            self.vector = numpy.array([x,y])
+        elif isinstance(position, (numpy.ndarray)):
+            #POSITION CAN ALSO BE PROVIDED AS A VECTOR(NUMPY ARRAY)
+            self.vector = position.copy()
+            chess_notation = ""
+            chess_notation += XVALUES[self.vector[0]]
+            chess_notation += YVALUES[self.vector[1]]
+            self.chess_notation = chess_notation
+    def print(self):
+        print(self.chess_notation)
+        print(self.vector)
+    def add_move(self, movement_vector):
+        if not isinstance(movement_vector, numpy.ndarray):
+            movement_vector = numpy.array(movement_vector)
+        return Position(self.vector + movement_vector)
+
 class Piece:
     ascii = {
         'PAWN':"P",
@@ -10,6 +41,7 @@ class Piece:
     def __init__(self, color="WHITE", piece="PAWN"):
         self.color = color
         self.piece = piece
+        self.piece_moves = piece_moves.move_methods[self.piece]
     def piece_ascii(self):
         piece_str = ""
         piece_str += self.color[0]
@@ -17,6 +49,26 @@ class Piece:
         return piece_str
     def get_ascii(self):
         return self.ascii[self.piece]
+    def eval_moves(self, position):
+        #REFACTOR THE EVAL
+        moves = self.piece_moves()
+        return_moves = {"moves":[], "captures":[]}
+        for m in moves["moves"]:
+            #CREATE A COPY OF THE ORIGINAL COORDINATES
+            #ADD THE X/Y VALUES OF A MOVE VECTOR TO THAT COORDINATE
+            if self.color == "BLACK":
+                m *= -1
+            new_position = position.add_move(m)
+        if validate_coordinates(new_position):
+            return_moves["moves"].append(new_position)
+        for c in moves["captures"]:
+            #CREATE A COPY OF THE ORIGINAL COORDINATES
+            #ADD THE X/Y VALUES OF A CAPTURE VECTOR TO THAT COORDINATE
+            new_position = position.add_move(c)
+            if validate_coordinates(new_position):
+                return_moves["captures"].append(new_position)
+            return return_moves
+    
         
 
 class Spot:
@@ -106,7 +158,17 @@ class Board:
         start_spot.empty()
         pass
 
-
+def take_turn(board, side):
+    #TAKES IN A BOARD WITH THE CURRENT STATE OF A CHESS GAME
+    #AND THE SIDE TO GO AS A BOOL (TRUE = BLACK, FALSE = WHITE)
+    #GETS COMMAND (EX. MOVE E7 E5) USING GET_STRING() OR GET_INPUT()
+    #USES SPLIT() TO BREAK THE COMMAND INTO THE NAME OF THE BOARD METHOD
+    #TO BE CALLED AND THE ARGUMENTS/PARAMETERS FOR THAT FUNCTION CALL
+    command = raw_input('%s: WHAT IS YOUR COMMAND?' % side)
+    command_array = command.split()
+    if command_array[0] == "MOVE":
+        board.move()
+    
 board = Board()
 board.setup_board()
 board.print_board()
@@ -114,3 +176,9 @@ print(board.translate_position("E7"))
 print(board.get_position("E7").get_ascii())
 board.move("H7", "H4")
 board.print_board()
+position = Position("H4")
+#moves = piece_moves.eval_moves(position, piece_moves.pawn_moves())
+#print(moves)
+moves = board.get_position("E7").occupant.eval_moves(Position("E7"))
+for m in moves["moves"]:
+    m.print()
---
+++ piece_moves.py
@@ -0,0 +1,104 @@
+import numpy
+
+def pawn_moves():
+    #GENERATE AND RETURN VECTORS THAT REPRESENT WHERE A PAWN CAN MOVE AND OR CAPTURE RELATIVE TO ITS CURRENT POSITION
+    moves = []
+    captures = []
+    moves.append(numpy.array([0,1]))
+    captures.append(numpy.array([1,1]))
+    captures.append(numpy.array([-1,1]))
+    return {"moves":moves, "captures":captures}
+
+def rook_moves():
+    #GENERATE AND RETURN VECTORS THAT REPRESENT WHERE A ROOK CAN MOVE AND OR CAPTURE RELATIVE TO ITS CURRENT POSITION
+    moves = []
+    captures = []
+    for i in range(0,8):
+        moves.append(numpy.array([i,0]))
+        moves.append(numpy.array([-i,0]))
+        moves.append(numpy.array([0,i]))
+        moves.append(numpy.array([0,-i]))
+        captures.append(numpy.array([i,0]))
+        captures.append(numpy.array([-i,0]))
+        captures.append(numpy.array([0,i]))
+        captures.append(numpy.array([0,-i]))
+    return {"moves":moves, "captures":captures}
+
+def bishop_moves():
+    #GENERATE AND RETURN VECTORS THAT REPRESENT WHERE A BISHOP CAN MOVE AND OR CAPTURE RELATIVE TO ITS CURRENT POSITION
+    moves = []
+    captures = []
+    for i in range(0,8):
+        moves.append(numpy.array([i,i]))
+        moves.append(numpy.array([-i,i]))
+        moves.append(numpy.array([i,-i]))
+        moves.append(numpy.array([-i,-i]))
+        captures.append(numpy.array([i,i]))
+        captures.append(numpy.array([-i,i]))
+        captures.append(numpy.array([i,-i]))
+        captures.append(numpy.array([-i,-i]))
+    return {"moves":moves, "captures":captures}
+
+def knight_moves():
+    #GENERATE AND RETURN VECTORS THAT REPRESENT WHERE A KNIGHT CAN MOVE AND OR CAPTURE RELATIVE TO ITS CURRENT POSITION
+    moves = numpy.array([[1,2],[2,1],[-1,2],[-2,1],[1,-2],[2,-1],[-1,-2],[-2,-1]])
+    captures = numpy.array([[1,2],[2,1],[-1,2],[-2,1],[1,-2],[2,-1],[-1,-2],[-2,-1]])
+    return {"moves":moves, "captures":captures}
+
+def queen_moves():
+    #GENERATE AND RETURN VECTORS THAT REPRESENT WHERE A QUEEN CAN MOVE AND OR CAPTURE RELATIVE TO ITS CURRENT POSITION
+    #BISHOP + ROOK
+    moves = []
+    captures = []
+    return {"moves":moves, "captures":captures}
+
+def king_moves():
+    #GENERATE AND RETURN VECTORS THAT REPRESENT WHERE A KING CAN MOVE AND OR CAPTURE RELATIVE TO ITS CURRENT POSITION
+    #EVERY COMBINATION OF 0,1, AND -1 EXCEPT 0,0
+    moves = []
+    captures = []
+    return {"moves":moves, "captures":captures}
+
+def validate_coordinates(position):
+    coordinates = position.vector
+    #CHECK IF A GIVEN PAIR OF COORDINATES ON THE BOARD MAPS TO A VALID LOCATION ON THE BOARD
+    if coordinates[0] >= 0 and coordinates[0] < 8 and \
+       coordinates[1] >= 0 and coordinates[1] < 8:
+        return True
+    else:
+        return False
+
+def eval_moves(position, moves):
+    #TAKE IN THE CURRENT POSITION AND A DICTIONARY OF MOVES AND CAPTURES.
+    #REPLACE ALL MOVES AND CAPTURES WITH THE RESULTING LOCATIONS ON THE BOARD
+    return_moves = {"moves":[], "captures":[]}
+    for m in moves["moves"]:
+        #CREATE A COPY OF THE ORIGINAL COORDINATES
+        #ADD THE X/Y VALUES OF A MOVE VECTOR TO THAT COORDINATE
+        new_position = [*position]
+        print(new_position)
+        print(m)
+        new_position[0] += m[0]
+        new_position[1] += m[1]
+        if validate_coordinates(new_position):
+            return_moves["moves"].append(new_position)
+    for c in moves["captures"]:
+        #CREATE A COPY OF THE ORIGINAL COORDINATES
+        #ADD THE X/Y VALUES OF A CAPTURE VECTOR TO THAT COORDINATE
+        new_position = [*position]
+        print(new_position)
+        print(c)
+        new_position[0] += c[0]
+        new_position[1] += c[1]
+        if validate_coordinates(new_position):
+            return_moves["captures"].append(new_position)
+    return return_moves
+
+move_methods = {
+    "PAWN":pawn_moves,
+    "ROOK":rook_moves,
+    "BISHOP":bishop_moves,
+    "KNIGHT":knight_moves,
+    "QUEEN":queen_moves,
+    "KING":king_moves
+}
